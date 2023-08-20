c61c7c5c09bf32475b2cdb52e06e68aa4bb97cea
adamreaksmey <adaminiature@gmail.com>
2023-08-20 18:18:50 +0700

    update serverside

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct(private Request $request)
+    {
+    }
+
+    public function login()
+    {
+        $email = $this->request->email;
+        $password = $this->request->password;
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $user = Auth::user();
+            $token = $user->createToken('auth-token')->plainTextToken;
+            return response()->json([
+                'token' => $token
+            ]);
+        }
+        return response()->json(['error' => 'Invalid email or password'], 401);
+    }
+
+    public function test()
+    {
+        return response()->json([
+            'message' => 'you are in'
+        ]);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/testing', [AuthController::class, 'test']);
+});
