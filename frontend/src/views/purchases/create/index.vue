b33e938e31f64814e9c7e914e6f92ec5e6bb0903
adamreaksmey <adaminiature@gmail.com>
2023-08-25 10:43:43 +0700

    form wrapped

--- frontend/src/views/purchases/create/index.vue
+++ frontend/src/views/purchases/create/index.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="container">
     <h1>Create Purchase</h1>
-    <div class="form-group">
-      <label for="name">Name:</label>
-      <select id="name" class="form-control" v-model="selectedData" @change="handleDropDown">
-        <option value="" disabled>Select a name</option>
-        <option v-for="user in userInfo" :value="user" :key="user.id">{{ user.name }}</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="name">Amount Purchased:</label>
-      <input type="number" id="amount" class="form-control" v-model="form.amount" />
-    </div>
-    <div class="form-group">
-      <label for="purchaseId">Customer ID:</label>
-      <input type="text" id="purchaseId" class="form-control" disabled v-model="form.customer_id" />
-    </div>
-    <div class="form-group">
-      <label for="createdAt">Created At:</label>
-      <input type="text" id="createdAt" class="form-control" disabled v-model="form.created_at" />
-    </div>
-    <CButton class="mt-2" color="primary" @click="createPurchase">Create Purchase</CButton>
+    <form @submit.prevent="createPurchase">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <select
+          id="name"
+          class="form-control"
+          v-model="selectedData"
+          @change="handleDropDown"
+          required
+        >
+          <option value="" disabled>Select a name</option>
+          <option v-for="user in userInfo" :value="user" :key="user.id">{{ user.name }}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="name">Amount Purchased:</label>
+        <input type="number" id="amount" class="form-control" v-model="form.amount" required />
+      </div>
+      <div class="form-group">
+        <label for="purchaseId">Customer ID:</label>
+        <input
+          type="text"
+          id="purchaseId"
+          class="form-control"
+          disabled
+          v-model="form.customer_id"
+        />
+      </div>
+      <div class="form-group">
+        <label for="createdAt">Created At:</label>
+        <input type="text" id="createdAt" class="form-control" disabled v-model="form.created_at" />
+      </div>
+      <CButton class="mt-2" color="primary" type="submit">Create Purchase</CButton>
+    </form>
   </div>
 </template>
 
