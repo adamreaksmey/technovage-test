c6e59804114bece6965bad9adcfd785e95544c0b
adamreaksmey <adaminiature@gmail.com>
2023-08-25 15:44:12 +0700

    update, removed some unused packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,15 +5,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
-import Cookies from 'js-cookie'
 import VueAwesomePaginate from 'vue-awesome-paginate'
 import 'vue-awesome-paginate/dist/style.css'
 
 const app = createApp(App)
-
-app.config.globalProperties.$axios = axios
-app.config.globalProperties.$cookies = Cookies
 
 app.use(router)
 app.use(store)
